f3edeeb70ee26c26d03952190455965a4ce162a2
Frogleim <92037197+Frogleim@users.noreply.github.com>
2024-10-08 22:08:36 +0400

    patch 08.10

--- core/bot.py
+++ core/bot.py
@@ -8,7 +8,7 @@ import pandas as pd
 import os
 
 client = Client()
-telegram_token = '7911524695:AAFPawr8FXJ1gLWAtSQ_jFKj9X_XZuSyKaY'
+telegram_token = os.environ['TELEGRAM_TOKEN']
 
 
 def send_signal(update: Update, context: CallbackContext):
@@ -18,18 +18,30 @@ def send_signal(update: Update, context: CallbackContext):
         print(crossover_result)
 
         if crossover_result[0] != 'Hold':
-            message = (f"Symbol: BTCUSDT\n⚠️Signal: {crossover_result[0]}\nPrice: {crossover_result[1]}\n"
-                       f"ADX: {crossover_result[2]}\nATR: {crossover_result[3]}")
+            message = (f"Symbol: BTCUSDT\n⚠️Signal: {crossover_result[0]}\nLong EMA: {crossover_result[5]}\n"
+                       f"Short EMA: {crossover_result[6]}"
+                       f"Price: {crossover_result[1]}\n"
+                       f"ADX: {crossover_result[2]}\nATR: {crossover_result[3]}\nRSI: {crossover_result[4]}")
             update.message.reply_text(message)
             current_price = float(client.futures_ticker(symbol='BTCUSDT')['lastPrice'])
-            result, pnl, close = ema_crossover.monitor_trade(current_price, crossover_result[3],
-                                                             position_type=crossover_result[0])
-            if pnl > 0:
-                update.message.reply_text(f'Trade finished successfully\nResult:🚀 {result} \nPNL: 🤑{pnl}\n'
-                                          f'Close price: {close}')
-            else:
-                update.message.reply_text(f'Trade was not successful😥\nResult:❌ {result} \nLoss: 🙁{pnl}\n'
-                                          f'Close price: {close}')
+            if crossover_result[0] == 'long':
+                result, pnl, close = ema_crossover.long_trade(current_price, crossover_result[3],
+                                                             )
+                if pnl > 0:
+                    update.message.reply_text(f'Trade finished successfully\nResult:🚀 {result} \nPNL: 🤑{pnl}\n'
+                                              f'Close price: {close}')
+                else:
+                    update.message.reply_text(f'Trade was not successful😥\nResult:❌ {result} \nLoss: 🙁{pnl}\n'
+                                              f'Close price: {close}')
+            if crossover_result[0] == 'short':
+                result, pnl, close = ema_crossover.short_trade(current_price, crossover_result[3],
+                                                             )
+                if pnl > 0:
+                    update.message.reply_text(f'Trade finished successfully\nResult:🚀 {result} \nPNL: 🤑{pnl}\n'
+                                              f'Close price: {close}')
+                else:
+                    update.message.reply_text(f'Trade was not successful😥\nResult:❌ {result} \nLoss: 🙁{pnl}\n'
+                                              f'Close price: {close}')
 
             time.sleep(60)
         else:
--- core/ema_crossover.py
+++ core/ema_crossover.py
@@ -1,9 +1,9 @@
 from binance.client import Client
-import pandas as pd
 import ta
 import time
 import requests
-import numpy as np
+from scipy.special import additional
+
 from socket_binance import fetch_btcusdt_klines
 
 
@@ -22,9 +22,6 @@ def get_credentials():
     return response.json()
 
 
-# cred_data = get_credentials()
-# api_key = cred_data['api_key']
-# api_secret = cred_data['api_secret']
 client = Client()
 symbol = 'BTCUSDT'
 interval = '15m'
@@ -33,21 +30,14 @@ adx_period = 14
 
 
 def calculate_ema():
-    # Fetch the kline data
+    """Calculating indicators"""
     df = fetch_btcusdt_klines(symbol, interval)
-
     if df.empty:
         print("No data fetched.")
         return None, None, None, None, None
-
-    # Calculate short and long EMA
     short_ema = df['close'].ewm(span=5, adjust=False).mean()
     long_ema = df['close'].ewm(span=13, adjust=False).mean()
-
-    # Close price for the previous period
     close_price = df['close'].iloc[-2]
-
-    # Calculate True Range (TR)
     df['previous_close'] = df['close'].shift(1)
     df['high_low'] = df['high'] - df['low']
     df['high_prev_close'] = abs(df['high'] - df['previous_close'])
@@ -57,37 +47,25 @@ def calculate_ema():
     loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()
     rs = gain / loss
     rsi = 100 - (100 / (1 + rs))
-
     rsi_sma = rsi.rolling(window=14).mean()
-
-    # True Range (TR) is the maximum of these three values
     df['true_range'] = df[['high_low', 'high_prev_close', 'low_prev_close']].max(axis=1)
-
-    # Calculate the ATR using a rolling window
     atr_period = 14
     df['ATR'] = df['true_range'].rolling(window=atr_period).mean()
 
     # Fetch the latest ATR value
     atr = df['ATR'].iloc[-1]
-
-    # Calculate ADX using ta-lib (adjust for your ta implementation)
     adx_period = 14
     adx = ta.trend.adx(df['high'], df['low'], df['close'], window=adx_period)
-
-    # Validate the ADX value
-
     print(
         f'Long EMA: {long_ema.iloc[-1]} Short EMA: {short_ema.iloc[-1]} ATR: {df["ATR"].iloc[-2]} ADX: {adx.iloc[-1]}'
         f' RSI: {rsi.iloc[-1]} RSI SMA: {rsi_sma.iloc[-1]}')
-
-    return long_ema, short_ema, close_price, adx, atr
+    return long_ema, short_ema, close_price, adx, atr, rsi
 
 
 def check_crossover():
-    long_ema, short_ema, close_price, adx, atr = calculate_ema()
+    """Check for signal with based strategy"""
+    long_ema, short_ema, close_price, adx, atr, rsi = calculate_ema()
     missing_data = {}
-
-    # Data validation
     if short_ema is None or len(short_ema) < 2:
         missing_data['short_ema'] = 'Missing or invalid'
     if long_ema is None or len(long_ema) < 2:
@@ -98,74 +76,59 @@ def check_crossover():
         missing_data['adx'] = 'Missing or invalid'
     if atr is None or float(atr) <= 0:
         missing_data['atr'] = 'Missing or invalid'
-
-    # Raise error if there's missing data
     if missing_data:
         raise ValueError(f"Missing or invalid crossover data: {missing_data}")
-
-    # Calculate crossovers
     crossover_sell = (short_ema.iloc[-2] < long_ema.iloc[-2]) and (short_ema.iloc[-1] > long_ema.iloc[-1])
     crossover_buy = (short_ema.iloc[-2] > long_ema.iloc[-2]) and (short_ema.iloc[-1] < long_ema.iloc[-1])
-    # rsi_crossover_sell = (rsi.iloc[-2] < rsi_sma.iloc[-2]) and (rsi.iloc[-1] > rsi_sma.iloc[-1])
-    # rsi_crossover_buy = (rsi.iloc[-2] > rsi_sma.iloc[-2]) and (rsi.iloc[-1] < rsi_sma.iloc[-1])
+    additional_indicator_long = (adx.iloc[-1] > 20) and (rsi.iloc[-1] > 50) and (atr > 120)
+    additional_indicator_short = (adx.iloc[-1] > 20) and (rsi.iloc[-1] < 50) and (atr > 120)
 
-    # Debug logging to track values
     print(f"ADX: {adx.iloc[-1]}, ATR: {atr}, Crossover Buy: {crossover_buy}, Crossover Sell: {crossover_sell}")
-
-    # Return based on conditions
-    if crossover_buy and adx.iloc[-1] > 20 and float(atr) > 65:
-        return ['long', close_price, adx.iloc[-1], atr]
-    elif crossover_sell and adx.iloc[-1] > 20 and float(atr) > 65:
-        return ['short', close_price, adx.iloc[-1], atr]
+    if crossover_buy and additional_indicator_long:
+        return ['long', close_price, adx.iloc[-1], atr, rsi, long_ema.iloc[-1], short_ema.iloc[-1]]
+    elif crossover_sell and additional_indicator_short:
+        return ['short', close_price, adx.iloc[-1], atr, rsi, long_ema.iloc[-1], short_ema.iloc[-1]]
     else:
-        # Return 'Hold' if neither buy nor sell conditions are met
-        return ['Hold', close_price, adx.iloc[-1], atr]
+        return ['Hold', close_price, adx.iloc[-1], atr, rsi, long_ema.iloc[-1], short_ema.iloc[-1]]
 
 
-def monitor_trade(close_price, atr, position_type='long'):
-    if position_type == 'long':
-        target_price = close_price + atr
 
-        stop_loss = close_price - atr
-    elif position_type == 'short':
-        target_price = close_price - atr
-        stop_loss = close_price + atr
-    else:
-        raise ValueError("position_type must be either 'long' or 'short'")
+def long_trade(entry_price, atr):
+    """Monitoring long trade"""
+    target_price = entry_price + atr
+    stop_loss = entry_price - atr
 
     while True:
         try:
-            # Fetch the current price
             current_price = float(client.futures_ticker(symbol='BTCUSDT')['lastPrice'])
         except Exception as e:
             print(f"Error fetching price: {e}")
-            time.sleep(1)  # Wait for a bit before retrying
+            time.sleep(1)
             continue
-
-        if position_type == 'long':
-            # Check if target price is hit (Profit in long)
-            print(f'Target price: {target_price}, Current price: {current_price} Stop loss: {stop_loss}')
-            if current_price >= target_price:
-                return 'Profit', atr, target_price
-
-            # Check if stop loss is hit (Loss in long)
-            elif current_price < stop_loss:
-                return 'Loss', -atr, stop_loss
-
-        elif position_type == 'short':
-            # Check if target price is hit (Profit in short)
-            print(f'Target price: {target_price}, Current price: {current_price} Stop loss: {stop_loss}')
-
-            if current_price <= target_price:
-                return 'Profit', atr, target_price
-
-            # Check if stop loss is hit (Loss in short)
-            elif current_price > stop_loss:
-                return 'Loss', -atr, stop_loss
-
-        # Optional: Sleep to avoid overwhelming the API with too many requests
+        print(f'Target price: {target_price}, Current price: {current_price} Stop loss: {stop_loss}')
+        if current_price >= target_price:
+            return 'Profit', atr, target_price
+        elif current_price <= stop_loss:
+            return 'Loss', -atr, stop_loss
         time.sleep(1)
 
+def short_trade(entry_price, atr):
+    """Monitoring short trade"""
+    target_price = entry_price - atr
+    stop_loss = entry_price + atr
+    while True:
+        try:
+            current_price = float(client.futures_ticker(symbol='BTCUSDT')['lastPrice'])
+        except Exception as e:
+            print(f"Error fetching price: {e}")
+            time.sleep(1)
+            continue
+        print(f'Target price: {target_price}, Current price: {current_price}')
+        if current_price <= target_price:
+            return 'Profit', atr, target_price
+        elif current_price > stop_loss:
+            return 'Loss', -atr, stop_loss
+        time.sleep(1)
 
 # def start_trade(signal=None, close_price=None):
 #
